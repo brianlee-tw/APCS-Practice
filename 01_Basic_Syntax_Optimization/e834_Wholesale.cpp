8f23737f4a95a4787903754c4f865668fb5d3fcd
brianlee-tw <apexlayers.dev@gmail.com>
2026-07-01 11:06:24 +0800

    e834

--- 01_Basic_Syntax_Optimization/e834_Wholesale.cpp
+++ 01_Basic_Syntax_Optimization/e834_Wholesale.cpp
@@ -2,7 +2,7 @@
 // APCS Complexity: O(N)
 // APCS Tag: IO Optimization, Conditionals, Loops
 // APCS Difficulty: 1
-// APCS Note: 
+// APCS Note: https://app.notion.com/p/e834-Wholesale-39043be958cd8051a9d3c5f451870bc4?v=36a43be958cd8075b3ac000c2c628f5d&source=copy_link
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,13 +12,12 @@ int main() {
     cin.tie(0);
 
     int m, t;
-    if (cin >> m) { // 先讀入每箱基準量
+    if (cin >> m) { 
         while (cin >> t) {
             if (t == 0) {
-                break; // 偵測到 0 結束
+                break;
             }
 
-            // 修正：應將原先的 10 全部改為變數 m
             if (t % m == 0) {
                 cout << t / m << '\n';
             } else {
