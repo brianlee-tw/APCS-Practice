803f31ab43a2f77ddc07262084f36791f2dbb9e2
brianlee-tw <apexlayers.dev@gmail.com>
2026-07-10 14:37:11 +0800

    c085

---
+++ 01_Basic_Syntax_Optimization/c085_Pseudo_Random_Numbers.cpp
@@ -0,0 +1,46 @@
+// APCS Title: c085. 00350 - Pseudo-Random Numbers
+// APCS Complexity: O(M)
+// APCS Tag: Math Theory, Array
+// APCS Difficulty: 2
+// APCS Note: https://app.notion.com/p/c085-Pseudo-Random-Numbers-39943be958cd801aa8cac43e4e0e74ae?v=36a43be958cd8075b3ac000c2c628f5d&source=copy_link
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int z, i, m, l, cnt = 0;
+
+    while(cin >> z >> i >> m >> l){
+
+        if (z == 0 && i == 0 && m == 0 && l == 0){
+            break;
+        }
+
+        cnt++;
+        int t = 1, a[10005] = {0};
+        a[l] = 1; 
+
+        while (true){
+
+            t++; 
+
+            int random = (z * l + i) % m;
+            l = random;
+            
+            if (a[random] == 0){
+                a[random] = t;
+            }
+            else{
+                // 直接相減即為正確週期
+                cout << "Case " << cnt << ": " << (t - a[random]) << '\n';
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
