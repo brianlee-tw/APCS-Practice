0c901d53d4bf90d40caf6b5e65085210238035f5
brianlee-tw <apexlayers.dev@gmail.com>
2026-07-11 11:37:50 +0800

    d673

---
+++ 01_Basic_Syntax_Optimization/d673_No_Problem.cpp
@@ -0,0 +1,43 @@
+// APCS Title: d673. 11608 - No Problem
+// APCS Complexity: O(1)
+// APCS Tag: IO Optimization, Loops, Array
+// APCS Difficulty: 2
+// APCS Note: https://app.notion.com/p/d673-No-Problem-39a43be958cd808c8228d90c85fb8035?v=36a43be958cd8075b3ac000c2c628f5d&source=copy_link
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int s, cnt = 0;
+
+    while (cin >> s) {
+        if (s < 0) break;
+
+        int a[12] = {0}, b[12] = {0}, remain = s;
+
+        cnt++;
+        cout << "Case " << cnt << ":\n";
+
+        for (int i = 0; i < 12; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < 12; i++) {
+            cin >> b[i];
+        }
+        
+        for (int i = 0; i < 12; i++) {
+            if (remain < b[i]) {
+                cout << "No problem. :(\n";
+            } else {
+                cout << "No problem! :D\n";
+                remain -= b[i];
+            }
+            remain += a[i];
+        }
+    }
+    return 0;
+}
